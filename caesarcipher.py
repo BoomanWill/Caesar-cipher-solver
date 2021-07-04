08fe5bd70a935c71a7ab3589bdf8f03bf994e1f1
BoomanWill <76132689+BoomanWill@users.noreply.github.com>
2021-07-04 18:35:57 +0100

    Add files via upload

---
+++ caesarcipher.py
@@ -0,0 +1,52 @@
+import time
+
+codedmessage = input('enter the coded message\n')
+wordtext = open('/home/will/Documents/Tools/words.txt')
+line = wordtext.readline()
+wordlist = []
+for line in wordtext:
+    word = line.strip()
+    wordlist.append(word)
+words = codedmessage.split()
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+solution = []
+successful = []
+unique = []
+thedict = {}
+
+for n in range(1, 52):
+    for w in words:
+        wordsolution = ''
+        for i in w:
+            index = alphabet.index(i)
+            index = index + n
+            letter = alphabet[index]
+            wordsolution += letter
+        print(wordsolution)
+        time.sleep(0.005)
+        if wordsolution in wordlist:
+            successful.append(n)
+            solution.append(wordsolution)
+        
+for i in successful:
+    if i not in unique:
+        unique.append(i)
+
+for u in unique:
+    if successful.count(u) >= len(words):
+        theshift = u
+
+solution = []
+
+
+for w in words:
+    wordsolution = ''
+    for i in w:
+        index = alphabet.index(i)
+        index += theshift
+        letter = alphabet[index]
+        wordsolution += letter
+    solution.append(wordsolution)
+
+print(solution)
